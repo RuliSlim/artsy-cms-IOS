edd8a14f87d36edccbee9b8c771ef430c638ad98
RuliSlim <ruli0to8@gmail.com>
2020-06-21 09:13:01 +0700

    Initial Commit

---
+++ artsy me up cms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  artsy me up cms
+//
+//  Created by Ruli on 21/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
